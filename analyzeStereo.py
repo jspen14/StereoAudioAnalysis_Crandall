5401d9fe2bf075c2c604908b74dbbfcb697d7d70
Jordan Henstrom <jhenstrom@gmail.com>
2019-10-11 16:09:56 -0600

    AsdAd

--- analyzeStereo.py
+++ analyzeStereo.py
@@ -2,35 +2,36 @@ import pyaudio
 import numpy as np
 import sys
 
-CHUNK = 2**11
+CHUNK = 2**11 # Look into this param see if affects resolution
 RATE = 48000
 LMIC_INDEX = 12
 RMIC_INDEX = 13
 MAX_VAL = 2**16
 
-def computeEqScalar(lrDifferences):
-    avg = np.average(lrDifferences)
-    print(avg)
-    sys.exit(1)
+def getInitialVolumes(streamL, streamR, iterations):
 
+    historicL = []
+    historicR = []
 
-def getInitialEqScalar(streamL, streamR):
-    lrDifferences = []
-
-    for i in range(100):
+    for i in range(iterations):
         dataL = np.fromstring(streamL.read(CHUNK, exception_on_overflow = False),dtype=np.int16)
         dataR = np.fromstring(streamR.read(CHUNK, exception_on_overflow = False),dtype=np.int16)
 
         peakL = np.average(np.abs(dataL))
         peakR = np.average(np.abs(dataR))
 
-        lrDifference = peakL-peakR
+        historicL.append(peakL)
+        historicR.append(peakR)
 
-        lrDifferences.append(lrDifference)
-
-    computeEqScalar(lrDifferences)
+    return historicL, historicR
 
 def main():
+    itersBeforeReInit = 100
+    significantDifference = 1.5
+    noiseFactor = 0
+
+    # Test to see if one mic can have multiple streams
+
     p = pyaudio.PyAudio()
 
     streamL = p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
@@ -39,21 +40,51 @@ def main():
     streamR = p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
                   frames_per_buffer=CHUNK, input_device_index=RMIC_INDEX)
 
-    eqScalar = getInitialEqScalar(streamL, streamR)
+    print "\nInitializing ..."
+    historicL, historicR = getInitialVolumes(streamL, streamR, itersBeforeReInit)
+    averageL = np.average(historicL)
+    stdDevL = np.std(historicL)
+    averageR = np.average(historicR)
+    stdDevR = np.std(historicR)
+    historicL = []
+    historicR = []
+    print "\nInitialization Complete."
 
     while True:
-
       dataL = np.fromstring(streamL.read(CHUNK, exception_on_overflow = False),dtype=np.int16)
       dataR = np.fromstring(streamR.read(CHUNK, exception_on_overflow = False),dtype=np.int16)
 
-      peakL = eqScalar*np.average(np.abs(dataL))
+      peakL = np.average(np.abs(dataL))
       peakR = np.average(np.abs(dataR))
 
-      if peakL - peakR > 1000:
-          if peakL > peakR:
-              print("LEFT")
+      spikeL = peakL > averageL*significantDifference
+      spikeR = peakR > averageR*significantDifference
+
+      if spikeL and spikeR:
+          normL = (peakL-averageL)/stdDevL
+          normR = (peakR-averageR)/stdDevR
+
+          if abs(normL - normR) < noiseFactor:
+              print("")
+          elif normL > normR:
+              print("L")
           else:
-              print("RIGHT")
+              print("R")
+
+      else:
+          historicL.append(peakL)
+          historicR.append(peakR)
+
+      if len(historicL) > itersBeforeReInit or len(historicR) > itersBeforeReInit:
+          averageL = np.average(historicL)
+          stdDevL = np.std(historicL)
+          averageR = np.average(historicR)
+          stdDevR = np.std(historicR)
+          historicL = []
+          historicR = []
+          print("ReINIT")
+
+
 
     stream.stop_stream()
     stream.close()
